b46d60d18e0447f3d3f27a196e9d56d3012b0652
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-08-02 19:25:24 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ const reviews = [
     name: 'moon byul-yi',
     job: 'ceo',
     img: 'https://i.pinimg.com/originals/44/96/2c/44962cc599431d7efb080b2a5295bfe8.jpg',
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    text: 'This app allows me to do my tasks from my smartphone where I want and when I want to. The interface is easy to navigate and I find everything I need quickly. I can’t wait for the new features'
   },
   {
     id: 2,
     name: 'ahn hye-jin',
     job: 'president',
     img: 'https://portalpopline.com.br/wp-content/uploads/2020/12/Hwasa.jpg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.'
+    text: 'Amazing app.  There are so many things to choose from.  It’s impossible to get bored. They eliminate any excuses because there are features for all levels, all time frames, no equipment options, small space options, and even options to have your kids join you. I truly love this app and have had several friends join as well.'
   },
   {
     id: 3,
     name: 'kim yong-sun',
     job: 'model',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGCrMKOphDmYKLSTpa0WWKWDA21zHOM2CLqg&usqp=CAU',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.'
+    text: 'This app is super useful and helpful for users that are not a professional yet. I liked how user-friendly the app is. I would recommend this app to anyone who is looking for a quick and easy way to learn.'
   },
   {
     id: 4,
     name: 'Jung whee-in,',
     job: 'dancer',
     img: 'http://pm1.narvii.com/7379/b6c55b9cf8f4d641cf5ec97c62646c41be548dbdr1-723-675v2_00.jpg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. '
+    text: 'I absolutely love this app! It works so well. I love that it has everything I wanted. Just make sure to save before exiting. . I’ve only had it for a month now but I’m super impressed with the quality! And the response was real quick!'
   }
 ]
 /* select items */
